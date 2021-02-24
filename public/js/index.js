abeb70a7f89b78613cf4da21ab169323d0ad8584
Alva Persson <ap223st@student.lnu.se>
2021-02-24 14:57:03 +0100

    Refactor code, less repeating

--- public/js/index.js
+++ public/js/index.js
@@ -3,21 +3,25 @@ import '../socket.io/socket.io.js'
 const socket = window.io()
 
 socket.on('issueClosed', arg => {
-  console.log('closed: ', arg.id)
-  const issue = document.querySelector(`#id${arg.id}`)
-  issue.firstElementChild.textContent = 'closed'
-  const form = document.querySelector(`#id${arg.id} form`)
-  form.setAttribute('action', `issues/${arg.id}/reopen`)
-  const button = document.querySelector(`#id${arg.id} button`)
-  button.textContent = 'REOPEN text'
+  changeStateOfIssue(arg.id, 'Closed', 'reopen')
 })
 
 socket.on('issueReopen', arg => {
-  console.log('reopen: ', arg)
-  const issue = document.querySelector(`#id${arg.id}`)
-  issue.firstElementChild.textContent = ''
-  const form = document.querySelector(`#id${arg.id} form`)
-  form.setAttribute('action', `issues/${arg.id}/close`)
-  const button = document.querySelector(`#id${arg.id} button`)
-  button.textContent = 'CLOSE text'
+  changeStateOfIssue(arg.id, '', 'close')
 })
+
+/**
+ * Called to change state of an issue.
+ *
+ * @param {number} id - The id of the issue.
+ * @param {string} statetext - The text to use to dispaly the state of the issue.
+ * @param {string} action - The action that is possible to perform on the issue.
+ */
+function changeStateOfIssue (id, statetext, action) {
+  const issue = document.querySelector(`#id${id}`)
+  issue.firstElementChild.textContent = statetext
+  const form = document.querySelector(`#id${id} form`)
+  form.setAttribute('action', `issues/${id}/${action}`)
+  const button = document.querySelector(`#id${id} button`)
+  button.textContent = action
+}
